84724c504066d5acc0ee95de08c4b3a736c8fdc2
Diana Guzman-Colon <dguzmancolon@russell.wisc.edu>
2018-05-16 16:13:52 -0500

    Pruned euc graph

--- hfnetworks.R
+++ hfnetworks.R
@@ -24,40 +24,46 @@ library(ggplot2)
 
 ####Load Adjacency matrix ####
 getwd()
+#setwd("Y:/dguzmancolon/igraph/igraph") #for Zap, pero las columnas no se alinean bien, y los node ID son raros.  
 
 nohu.data<-gdata::read.xls("V:/dguzmancolon/dguzmancolon/igraph/igraph/NoHU_resistances.xls", sheet=1, perl=perl, header=TRUE)
+    #nohu.data<-read.csv("./NoHU_resistances.csv", header=TRUE, sep="") #zap
 nohu.nodes<-names(nohu.data)[-1] #get node IDs from first column
 #remove blank names in nodes
 #nohu.nodes<-nohu.nodes[-(153:159)]
 
+#Import and convert into adjacency matrix
 nohu.mat<-data.matrix(nohu.data, rownames.force=TRUE)#Import and convert into adjacency matrix
 nohu.mat<-nohu.mat[,-1] #The matrix should include the first row (which is data),
                         #but not the first column (which too contains node-names).
 #to remove NA's: 
 #nohu.mat<-nohu.mat[ , ! apply( nohu.mat , 2 , function(x) all(is.na(x)) ) ] 
 
-#As the matrix is now of the size N by N, row- and colnames makes for a neat matrix
+#As the matrix is now of the size N by N, row- and colnames makes for a neat matrix. Row names need to be 
+#the same as nodes names from the column, below is the code for that#
 rownames(nohu.mat) <- colnames(nohu.mat) <- nohu.nodes 
 #Check
 nohu.mat
 
 #####Exmaple load edges list####
-el=read.csv("Z:/HumanInfluence/HFCircuitscape/HF_EucledianCores/distances_HFCores_NoHUResamp_cores_2.txt", 
+el.euc=read.csv("Z:/HumanInfluence/HFCircuitscape/HF_EucledianCores/distances_HFCores_NoHUResamp_cores_2.txt", 
             sep="", header=FALSE) # read  the 'el.with.weights.csv' file
-#names(el) <- c("V1","V2","Weight") ##add column names
+names(el.euc) <- c("V1","V2","weight") ##add column names
 
-el[,1]=as.character(el[,1]) #Because the vertex IDs in this dataset are numbers, we make sure igraph knows these should be treated as characters. Otherwise, it'll create problems (see page on data import)
-el[,2]=as.character(el[,2])
+el.euc[,1]=as.character(el.euc[,1]) #Because the vertex IDs in this dataset are numbers, we make sure igraph knows these should be treated as characters. Otherwise, it'll create problems (see page on data import)
+el.euc[,2]=as.character(el.euc[,2])
 
-el.mat<-el[,-3]
+el.mat<-el.euc[,-3]
 el.mat=as.matrix(el.mat) #igraph needs the edgelist to be in matrix format
-g=graph.edgelist(el.mat[,1:2], directed=FALSE) #We first greate a network from the first two columns, which has the list of vertices
-E(g)$Weight=as.numeric(el[,3]) #We then add the edge weights to this network by assigning an edge attribute called 'weight'.
+euc.g=graph.edgelist(el.mat[,1:2], directed=FALSE) #We first greate a network from the first two columns, which has the list of vertices
+E(euc.g)$weight=as.numeric(el[,3]) #We then add the edge weights to this network by assigning an edge attribute called 'weight'.
 
-plot(g,layout=layout.fruchterman.reingold,edge.width=E(g)$Weight/2)
+
+
+plot(euc.g,layout=layout.fruchterman.reingold,edge.width=E(euc.g)$weight/100000)
 
 #To get adjecency matrix from network
-el.adj=get.adjacency(g,attr= 'Weight') #attr='weight' makes sure that the weights are shown in the adjacency matrix. 
+el.adj=get.adjacency(euc.g,attr= 'weight') #attr='weight' makes sure that the weights are shown in the adjacency matrix. 
 el.adj
 
 
@@ -67,16 +73,17 @@ el.adj
 Wnohu.graph=igraph::graph_from_adjacency_matrix(nohu.mat,mode="undirected",weighted=TRUE,diag=FALSE) ###weighted
 #Weuc.copy=get.adjacency(Weuc.graph.copy,attr= 'Weight')
 #Weuc.copy.g<-igraph::graph_from_adjacency_matrix(Weuc.copy,mode="undirected",weighted=TRUE,diag=FALSE)
-#Weuc.graph<- igraph::graph_from_adjacency_matrix(el.adj,mode="undirected",weighted=TRUE,diag=FALSE)
+Weuc.g<- igraph::graph_from_adjacency_matrix(el.adj,mode="undirected",weighted=TRUE,diag=FALSE)
 #Weuc.graph1 <- graph_from_edgelist(el.mat, directed=FALSE)
 #view name of nodes(vertices)
 #V(Wnohu.graph)
 #V(Weuc.graph)
 #V(Weuc.copy.g)
+V(Weuc.g)
 #Weuc.graph
 #Weuc.copy.g
 #E(Weuc.graph)$Weight=as.numeric(el[,3])
-E(Weuc.copy.g)$weight
+E(Weuc.g)$weight
 
 #load Node attribute table#
 node_attr<-read.table("V:/dguzmancolon/dguzmancolon/igraph/igraph/CoreProp.txt", header=TRUE, sep = "\t")
@@ -107,12 +114,19 @@ v.size=as.numeric(v.size)
 #v.size=(V(Weuc.copy.g)$size)
 #v.size=as.numeric(v.size)
 
+#V(Weuc.g)$AREA=as.character(node_attr$AREA[match(V(Weuc.g)$name,node_attr$core_ID)])
+#V(Weuc.g)$AREA<-V(Weuc.g)$AREA[!is.na(V(Weuc.g)$AREA)] #remove NA's?
+#V(Weuc.g)$AREA
+#V(Weuc.g)$size=V(Weuc.g)$AREA
+#v.size=(V(Weuc.g)$size)
+#v.size=as.numeric(v.size)
+
 ##edges
 E(Weuc.graph)$weight<-E(Weuc.graph)$weight[!is.na(E(Weuc.graph)$weight)]##remove NA's?
 E(Weuc.graph)$weight
 
-E(Weuc.copy.g)$weight<-E(Weuc.copy.g)$weight[!is.na(E(Weuc.copy.g)$weight)]##remove NA's?
-E(Weuc.copy.g)$weight
+E(Weuc.g)$weight<-E(Weuc.g)$weight[!is.na(E(Weuc.g)$weight)]##remove NA's?
+E(Weuc.g)$weight
 
 ## Add coordinates to nodes#
 #You can add the coordinates directly to the graph object in igraph by setting the an X and Y attributes to the vertices
@@ -394,18 +408,29 @@ try({
   print(modularity(fc))
 }, silent=TRUE)
 
-###Graphs###
+####Graphs#####
+
+##Graph#
+cut.off <- 95541
+  max(E(Weuc.g)$weight)
+net.sp <- delete_edges(Weuc.g, E(Weuc.g)[weight>cut.off])
+plot(net.sp,vertex.size=v.size/1000000, vertex.label.cex=c(0.5), layout=coords, rescale=T, edge.width=E(Weuc.g)$weight/10000000)
+
+modular <- cluster_optimal(net.sp)
+modular
+
 #Degree distribution#
-#Weuc.copy.g.copy.deg <- degree(Weuc.copy.g.copy) # List of degrees
-Wnohu.graph.copy.deg <- degree(Wnohu.graph.copy)
-#Weuc.copy.g.copy.histogram <- as.data.frame(table(Weuc.copy.g.copy.deg)) # Let's count the frequencies of each degree
-Wnohu.graph.copy.histogram <- as.data.frame(table(Wnohu.graph.copy.deg))
+net.sp.deg <- degree(net.sp) # List of degrees
+#Wnohu.graph.copy.deg <- degree(Wnohu.graph.copy)
+net.sp.hist <- as.data.frame(table(net.sp.deg)) # Let's count the frequencies of each degree
+#Wnohu.graph.copy.histogram <- as.data.frame(table(Wnohu.graph.copy.deg))
+
 # Need to convert the first column to numbers, otherwise
 # the log-log thing will not work (that's fair...)
-#Weuc.copy.g.copy.histogram[,1] <- as.numeric(Weuc.copy.g.copy.histogram[,1])
-Wnohu.graph.copy.histogram[,1] <- as.numeric(Wnohu.graph.copy.histogram[,1])
+net.sp.hist[,1] <- as.numeric(net.sp.hist[,1])
+#Wnohu.graph.copy.histogram[,1] <- as.numeric(Wnohu.graph.copy.histogram[,1])
 # Now, plot it!
-ggplot(Wnohu.graph.copy.histogram, aes(x = Wnohu.graph.copy.deg, y = Freq)) +
+ggplot(net.sp.hist, aes(x = net.sp.deg, y = Freq)) +
   geom_point() +
   scale_x_continuous("Degree\n(nodes with this amount of connections)",
                      breaks = c(1, 3, 10, 30, 100, 300),
@@ -416,11 +441,218 @@ ggplot(Wnohu.graph.copy.histogram, aes(x = Wnohu.graph.copy.deg, y = Freq)) +
   ggtitle("Degree Distribution (log-log)") +
   theme_bw()
 
-##Graph#
-cut.off <- 0.6
-  #mean(E(Wnohu.graph.copy)$weight)
-net.sp <- delete_edges(Wnohu.graph.copy, E(Wnohu.graph.copy)[weight>cut.off])
-plot(net.sp,vertex.size=v.size/1000000, vertex.label.cex=c(0.5), layout=coords, rescale=T, edge.width=E(Wnohu.graph.copy)$weight)
+#####Euc graphs####
+# calculate the number of connections per node
+# calculate the clustering of each node
+net.sp
+k<-degree(net.sp)
+C <- transitivity(net.sp, type = "weighted")
+L <- mean_distance(net.sp)
+Pk<-degree(net.sp)
 
-modular <- cluster_optimal(net.sp)
-modular
+#Weuc.copy.g.copy
+#k<-degree(Weuc.copy.g.copy)
+#C <- transitivity(Weuc.copy.g.copy, type = "weighted")
+#L <- mean_distance(Weuc.copy.g.copy)
+#Pk<-degree(Weuc.copy.g.copy)
+
+# mean clustering of all nodes with k links
+Ck <- rep(0, max(k))
+for (i in 1:max(k)){
+  Ck[i] <- mean(C[k==i], na.rm = TRUE)
+}
+plot(Ck)
+
+##Randomize your network###
+Rnet.sp <- sample_degseq(Pk, in.deg=NULL, method="simple")
+# Calculate C and L
+CR <- transitivity(Rnet.sp, type="average")
+LR <- mean_distance(Rnet.sp)
+# Compare yours with random
+(C/CR)/(L/LR) >= 0.012*vcount(Rnet.sp)^1.11
+
+#Check if graph is small world###
+# Input: an igraph object
+# Outputs: TRUE/FALSE presence of small-workd and score
+
+is.smallworld = function(graph, rep=1000){
+  
+  # function that compares the C and L of empirical and random equivalent networks
+  test.sw = function(graph){
+    deg <- degree(graph)
+    Crand <- matrix(0,nrow=1, ncol=rep)
+    Lrand <- matrix(0,nrow=1, ncol=rep)
+    for (i in 1:rep){
+      # generate random equivalent network
+      Grand <- degree.sequence.game(deg, method="simple")
+      # calculate C and L
+      Crand[i] <- transitivity(Grand, type="average", isolates="NaN")
+      Lrand[i] <- average.path.length(Grand)
+    }
+    # C and L empirical and the average of the random ones
+    C <- transitivity(graph, type="average", isolates="NaN")
+    L <- average.path.length(graph)
+    CR <- mean(Crand, na.rm=TRUE)
+    LR <- mean(Lrand, na.rm=TRUE)
+    smallwordness <- (C/CR)/(L/LR)
+    smallwordness
+  }
+  
+  # check if the graph is connected
+  check1 <- is.connected(graph)
+  # if connected: calculate small-world
+  if (check1==TRUE){
+    print("This graph is connected")
+    print(paste("Is small-world?", test.sw(graph)>=0.012*vcount(graph)^1.11))
+    print(paste("score =", round(test.sw(graph), 3)))
+    # if not connected: calculate small-world for each cluster 
+  } else{
+    print("This graph is not connected")
+    for (i in 1:no.clusters(graph)){
+      subgraph <- induced.subgraph(graph, v=clusters(graph)$membership==i)
+      print(paste("Cluster", i))
+      print(paste("Is small-world?", test.sw(subgraph)>=0.012*vcount(subgraph)^1.11))
+      print(paste("score =", round(test.sw(subgraph), 3)))
+    }
+  }
+}
+
+is.smallworld(net.sp)
+
+#Scale-free##
+degree = 1:max(degree(net.sp, mode = "all"))
+dd = degree_distribution(net.sp, mode="all", cumulative=T)
+probability = dd[-1]
+# delete 0s
+nonzero.position = which(probability != 0)
+probability = probability[nonzero.position]
+degree = degree[nonzero.position]
+# linear fit of logarithms
+reg = lm(log(probability) ~ log(degree))
+cozf = coef(reg)
+power.law.fit = function(x) exp(cozf[[1]] + cozf[[2]] * log(x))
+# alpha and r
+alpha = -cozf[[2]]
+R.square = summary(reg)$r.squared
+# linear fit of logarithms
+reg = lm(log(probability) ~ log(degree))
+cozf = coef(reg)
+power.law.fit = function(x) exp(cozf[[1]] + cozf[[2]] * log(x))
+# alpha and r
+alpha = -cozf[[2]]
+R.square = summary(reg)$r.squared
+
+print(paste("Alpha =", round(alpha, 3)))
+print(paste("R square =", round(R.square, 3)))
+
+#plot
+plot(probability~degree, log="xy", xlab="ki", ylab="P(k)", col=1)
+curve(power.law.fit, col="red", add=T, n=length(degree))
+
+# Function to estimate if network is scale-free##
+# Input: an igraph object, cumulative TRUE/FALSE
+# Outputs: TRUE (r > 0.7) / FALSE, alpha and r
+
+is.scalefree = function(graph, cumulative = c(TRUE, FALSE)){
+  d <- degree(graph, mode = "all")
+  # Fit P(k) to power-law
+  # initialize vectors
+  dd = degree.distribution(graph, mode = "all", cumulative = cumulative)
+  probability = dd[-1]
+  degree = 1:max(d)
+  # delete blank values
+  nonzero.position = which(probability != 0)
+  probability = probability[nonzero.position]
+  degree = degree[nonzero.position]
+  # linear fit of logarithms
+  reg = lm(log(probability) ~ log(degree))
+  cozf = coef(reg)
+  power.law.fit = function(x) exp(cozf[[1]] + cozf[[2]] * log(x))
+  alpha = -cozf[[2]]
+  R.square = summary(reg)$r.squared
+  # output
+  print(paste("Is scale-free?", R.square>0.7))
+  print(paste("Alpha =", round(alpha, 3)))
+  print(paste("R square =", round(R.square, 3)))
+  dev.new()
+  plot(probability ~ degree, log = "xy", xlab = "Connections (log)", ylab = "Probability (log)", col = 1, main = "Connectivity Distribution P(k)")
+  curve(power.law.fit, col = "red", add = T, n = length(d)) 
+}
+
+scalefree<-is.scalefree(net.sp, cumulative = c(TRUE))
+#Is my Network Hierarchical?###
+#The script is the same, only that instead of frequency now we calculate the average C of nodes with the same k
+
+#cluster = transitivity(graph, type="local", isolates="NaN")
+cluster = transitivity(net.sp, type="local", isolates="NaN")
+d=degree(net.sp)
+
+for (i in 1:max(d)){
+  probability[i]=mean(cluster[d==i], na.rm=TRUE)
+}
+
+#Function to estimate if network is hierarchical
+# Input: an igraph object, cumulative Pk TRUE/FALSE
+# Outputs: TRUE (r > 0.7) / FALSE, alpha and r
+
+is.hierarchy = function(graph, cumulative = c(TRUE, FALSE)){
+  d <- degree(graph, mode = "all")
+  dd = degree.distribution(graph, mode = "all", cumulative = cumulative)
+  
+  # initialize vectors
+  probability = dd[-1]
+  degree = 1:max(d)
+  # delete blank values
+  nonzero.position = which(probability != 0)
+  probability = probability[nonzero.position]
+  degree = degree[nonzero.position]
+  # linear fit of logarithms
+  reg = lm(log(probability) ~ log(degree))
+  cozf = coef(reg)
+  power.law.fit = function(x) exp(cozf[[1]] + cozf[[2]] * log(x))
+  
+  alpha1 = -cozf[[2]]
+  R.square1 = summary(reg)$r.squared
+  
+  # Fit C(k) to power-law
+  cluster = transitivity(graph, type = "local", isolates = "NaN")
+  for (i in 1:max(d)) {probability[i] = mean(cluster[d==i], na.rm=TRUE)}	
+  # delete blank values
+  nonzero.position = which(probability != 0)
+  probability = probability[nonzero.position]
+  degree = degree[nonzero.position]
+  # linear fit of logarithms
+  reg = lm(log(probability) ~ log(degree))
+  cozf = coef(reg)
+  power.law.fit = function(x) exp(cozf[[1]] + cozf[[2]] * log(x))
+  
+  alpha2 = -cozf[[2]]
+  R.square2 = summary(reg)$r.squared
+  
+  
+  print(paste("Is hierarchical?", (R.square1>0.7)&&(R.square2>0.7)))
+  
+  print("P(k) fit to Power-Law")
+  print(paste("Alpha =", round(alpha1, 3)))
+  print(paste("R square =", round(R.square1, 3)))
+  
+  print("C(k) fit to Power-Law")
+  print(paste("Alpha =", round(alpha2, 3)))
+  print(paste("R square =", round(R.square2, 3)))
+  
+}
+
+#is.hierarchy(Wnohu.graph.copy) 
+is.hierarchy(net.sp) 
+
+plot(probability~degree, log="xy", xlab="ki", ylab="C(k)", col=1)
+curve(power.law.fit, col="red", add=T, n=length(d))
+
+#to expoert as adjacency#
+#matrix packg for sparse matrices#
+install.packages('Matrix')
+library(Matrix)
+net.sp.export<-as_adjacency_matrix(net.sp, type = c("both"), attr = "weight",
+                    edges = TRUE, names = TRUE, sparse = igraph_opt("sparsematrices"))
+net.sp.expoert<-as.matrix(net.sp.export)
+write.csv(net.sp.expoert, "V:/dguzmancolon/dguzmancolon/igraph/igraph/eucPrunedgraph.csv", sep = "\t")
